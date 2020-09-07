2ad2c3ffbbfc16f2c16a37dec7e70fc84dfcfd46
Dev D <devdeepak.deepak@gmail.com>
2020-09-07 19:07:44 +0100

    Add Scripts

---
+++ move_sample_images.py
@@ -0,0 +1,56 @@
+# %%[markdown]
+"""
+# Move Sample Images
+- We would like to move the images that we have sampled and saved in the csv file `styles_sample.csv` to different folders for each of the following categories:
+    - by `Topwear`, `Shoes`, `Bottomwear`, `Headwear`
+    - further categorized by `Men` & `Women`
+"""
+# %%
+import pandas as pd
+import glob
+import os
+import shutil
+
+# %%
+df = pd.read_csv(
+    os.path.join('dataset', 'styles_sample.csv',),
+    index_col='id',
+    dtype={
+        'gender': 'category',
+        'masterCategory': 'category',
+        'subCategory': 'category',
+        'articleType': 'category',
+        'baseColour': 'category',
+        'season': 'category',
+        'productDisplayName': 'category',
+    },
+).sort_index(ascending=True)
+
+df.info()
+
+# %%
+
+for ind, row in df.iterrows():
+    source = os.path.join(
+        'dataset',
+        'images',
+        f"{ind}.jpg",
+    )
+    gender = row['gender']
+    sub_catergory = row['subCategory']
+    destination = os.path.join(
+        'dataset',
+        'image_sample',
+        sub_catergory,
+        gender,
+    )
+
+    if not os.path.isdir(destination):
+        os.makedirs(destination)
+
+    shutil.copy(source, destination)
+
+# %%
+
+print(os.path.isdir(destination))
+print(destination)
