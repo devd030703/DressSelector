5484cd9b146503d07fa7a11c03f54c7e2ec20641
Gabriel Harris <dr.gabriel.harris@gmail.com>
2020-10-12 20:01:12 +0100

    Update query_db.py

--- query_db.py
+++ query_db.py
@@ -11,7 +11,7 @@ class DataBase:
         self.cursor = self.cnxn.cursor()
 
     def check_user_exists_using_email(self, email):
-        row = self.cursor.execute("SELECT * FROM USER WHERE email=?",
+        row = self.cursor.execute("SELECT * FROM USERS WHERE Email=?",
                                   [email]).fetchone()
         if row:
             return True
@@ -19,7 +19,7 @@ class DataBase:
             return False
 
     def check_user_exists_using_rowid(self, rowid):
-        row = self.cursor.execute("SELECT * FROM USER WHERE rowid=?",
+        row = self.cursor.execute("SELECT * FROM USERS WHERE rowid=?",
                                   [rowid]).fetchone()
         if row:
             return True
@@ -27,58 +27,66 @@ class DataBase:
             return False
 
     def check_user_password_is_correct(self, email, password):
-        row = self.cursor.execute("SELECT * FROM USER WHERE email=?",
+        row = self.cursor.execute("SELECT * FROM USERS WHERE Email=?",
                                   [email]).fetchone()
-        if row[3] == password:
+        if row[4] == password:
             return True
         else:
             return False
 
-    def update_user_surname(self, rowid, surname):
-        self.cursor.execute("UPDATE USER SET surname=? WHERE rowid=?",
-                            (surname, rowid))
+    def update_user_first_name(self, rowid, first_name):
+        self.cursor.execute("UPDATE USERS SET FirstName=? WHERE rowid=?",
+                            (first_name, rowid))
         print(f'{self.cursor.rowcount} record(s) were modified...')
         self.cnxn.commit()
 
-    def update_user_forename(self, rowid, forename):
-        self.cursor.execute("UPDATE USER SET forename=? WHERE rowid=?",
-                            (forename, rowid))
+    def update_user_last_name(self, rowid, last_name):
+        self.cursor.execute("UPDATE USERS SET LastName=? WHERE rowid=?",
+                            (last_name, rowid))
         print(f'{self.cursor.rowcount} record(s) were modified...')
         self.cnxn.commit()
 
     def update_user_email(self, rowid, new_email):
-        self.cursor.execute("UPDATE USER SET email=? WHERE rowid=?",
+        self.cursor.execute("UPDATE USERS SET Email=? WHERE rowid=?",
                             (new_email, rowid))
         print(f'{self.cursor.rowcount} record(s) were modified...')
         self.cnxn.commit()
 
     def update_user_password(self, rowid, new_password):
-        self.cursor.execute("UPDATE USER SET password=? WHERE rowid=?",
+        self.cursor.execute("UPDATE USERS SET Password=? WHERE rowid=?",
                             (new_password, rowid))
         print(f'{self.cursor.rowcount} record(s) were modified...')
         self.cnxn.commit()
 
-    def insert_new_user(self, surname, forename, email, password):
-        self.cursor.execute("INSERT INTO USER VALUES (?, ?, ?, ?, 'local')",
-                            (surname, forename, email, password))
+    def insert_new_user(self, first_name, last_name, gender, email, password):
+        self.cursor.execute("INSERT INTO USERS VALUES (?, ?, ?, ?, ?)",
+                            (first_name, last_name, gender, email, password))
         print(f'{self.cursor.rowcount} record(s) were modified...')
         self.cnxn.commit()
 
-    def get_user_info(self, rowid):
-        return self.cursor.execute("SELECT rowid, * FROM USER WHERE rowid=?",
+    def get_user_info_using_rowid(self, rowid):
+        return self.cursor.execute("SELECT rowid, * FROM USERS WHERE rowid=?",
                                    [rowid]).fetchone()
 
     def get_user_info_using_email(self, email):
-        return self.cursor.execute("SELECT rowid, * FROM USER WHERE email=?",
+        return self.cursor.execute("SELECT rowid, * FROM USERS WHERE Email=?",
                                    [email]).fetchone()
 
-    def delete_user_shoot(self, user_rowid, shoot_rowid):
-        self.cursor.execute("""
-        DELETE FROM SHOOT 
-        WHERE  user_id IN (SELECT rowid 
-                           FROM   USER 
-                           WHERE  rowid = ?) 
-               AND rowid = ?;  
-                            """, (user_rowid, shoot_rowid))
+    def delete_user(self, user_rowid):
+        self.cursor.execute("DELETE FROM USERS WHERE rowid = ?", 
+                            (user_rowid))
         print(f'{self.cursor.rowcount} record(s) were modified...')
         self.cnxn.commit()
+
+    def insert_outfit(self, user_rowid):
+        self.cursor.execute("INSERT INTO OUTFITS VALUES (?)",
+                            (user_rowid))
+        print(f'{self.cursor.rowcount} record(s) were modified...')
+        self.cnxn.commit()
+
+    def insert_outfit(self, user_rowid):
+        self.cursor.execute("INSERT INTO OUTFITS VALUES (?)",
+                            (user_rowid))
+        print(f'{self.cursor.rowcount} record(s) were modified...')
+        self.cnxn.commit()
+
