aa5ab647fd18f92e43d66db6a54b12790f1a04aa
Gabriel Harris <dr.gabriel.harris@gmail.com>
2020-11-02 20:09:53 +0000

    Update app.py

--- dash/app.py
+++ dash/app.py
@@ -6,6 +6,8 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 
 import dash
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 
 # %%
 # ---------------------------------------- DATA ----------------------------------------
@@ -15,7 +17,7 @@ logo_image = os.path.join("images", "models.png")
 # ---------------------------------------- APP -----------------------------------------
 app = dash.Dash(
     __name__,
-    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    external_stylesheets=[dbc.themes.FLATLY],
 )
 
 # --------------------------------------- IMAGES ---------------------------------------
@@ -42,23 +44,27 @@ card_login = dbc.Card(
                     id="input_user_name",
                     placeholder="user name",
                     type="text",
-                    className="mb-5",
+                    className="mb-3",
                 ),
                 dbc.Input(
                     id="input_password",
                     placeholder="password",
                     type="text",
-                    className="mb-5",
+                    className="mb-3",
                 ),
                 dbc.Row(
                     [
                         dbc.Button(
-                            "login",
+                            children="login",
+                            id="button_login",
                             color="primary",
+                            className="m-3",
                         ),
                         dbc.Button(
-                            "signup",
+                            children="signup",
+                            id="button_signup",
                             color="primary",
+                            className="m-3",
                         ),
                     ]
                 ),
@@ -69,6 +75,13 @@ card_login = dbc.Card(
     inverse=True,
 )
 
+alert = dbc.Alert(
+    "Hello! I am an alert",
+    id="alert",
+    dismissable=True,
+    is_open=False,
+)
+
 # --------------------------------------- LAYOUT ---------------------------------------
 app.layout = dbc.Container(
     [
@@ -76,20 +89,50 @@ app.layout = dbc.Container(
             [
                 # row1: logos
                 dbc.Row(
-                    dbc.Col(img_ds),
+                    dbc.Col(
+                        html.Div("TITLE"),
+                        width={"size": 6, "offset": 3},
+                    ),
                     align="center",
-                    className="mb-5",
+                    className="m-5",
                 ),
                 # row2: login
                 dbc.Row(
-                    dbc.Col(card_login, width={"size": 6, "offset": 3}),
+                    dbc.Col(
+                        card_login,
+                        width={"size": 6, "offset": 3},
+                    ),
                     align="center",
-                    className="mb-5",
+                    className="m-5",
                 ),
+                # row3: alert
+                dbc.Row(alert),
             ]
         )
     ]
 )
+
+
+# ------------------------------------- CALLBACKS --------------------------------------
+# %%
+@app.callback(
+    Output("alert", "is_open"),
+    [
+        Input("button_login", "n_clicks"),
+    ],
+    [
+        State("input_user_name", "value"),
+        State("input_password", "value"),
+    ],
+)
+def login(
+    login_button_n_clicks,
+    input_user_name_value,
+    input_password_value,
+):
+    if login_button_n_clicks:
+        return True
+
 
 # %%
 if __name__ == "__main__":
