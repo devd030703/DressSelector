da2088e2fead64975319496ba6546a470d5b3c70
Gabriel Harris <dr.gabriel.harris@gmail.com>
2020-12-21 20:31:26 +0000

    Update signupPage.py

--- signupPage.py
+++ signupPage.py
@@ -1,8 +1,8 @@
 import base64
-import time
 
 import dash_bootstrap_components as dbc
 import dash_html_components as html
+from dash import callback_context
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 
@@ -69,11 +69,17 @@ card_login = dbc.Card(
                 dbc.Row(
                     [
                         dbc.Button(
+                            children="back",
+                            id="button_back",
+                            color="primary",
+                            className="m-3",
+                        ),
+                        dbc.Button(
                             children="create",
                             id="button_create",
                             color="primary",
                             className="m-3",
-                        )
+                        ),
                     ]
                 ),
             ]
