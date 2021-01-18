fe4bbedbc905bf612817b21e0cdf704f536ded2f
devd030703 <69916381+devd030703@users.noreply.github.com>
2021-01-18 18:07:38 +0000

    created sidebar

--- selectorPage_app_dev.py
+++ selectorPage_app_dev.py
@@ -207,6 +207,56 @@ user_details = (
     ),
 )
 
+# --------------------------------------- SIDE BAR ----------------------------------------
+
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        dbc.Button(
+            "Open collapse", id="collapse-button", className="mb-3", color="primary",
+        ),
+        dbc.Collapse(
+            [
+                html.H2("Sidebar", className="display-4"),
+                html.Hr(),
+                html.P(
+                    "A simple sidebar layout with navigation links", className="lead"
+                ),
+                dbc.Nav(
+                    [
+                        dbc.NavLink("Dress Selector", href="/", active="exact"),
+                        dbc.NavLink("Saved Dresses", href="/page-1", active="exact"),
+                        dbc.NavLink("Account Settings", href="/page-2", active="exact"),
+                    ],
+                    vertical=True,
+                    pills=True,
+                ),
+            ],
+            id="collapse",
+            style=SIDEBAR_STYLE,
+        ),
+    ],
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
 
 # --------------------------------------- LAYOUT ---------------------------------------
 app.layout = dbc.Container(
@@ -215,26 +265,34 @@ app.layout = dbc.Container(
             [
                 dbc.Row(
                     [
-                        dbc.Col(headwear, width=3,),
-                        dbc.Col(topwear, width=3,),
-                        dbc.Col(user_details, width=3,),
-                    ],
-                    justify="centre",
+                        dbc.Col(headwear,),
+                        dbc.Col(topwear,),
+                        dbc.Col(bottomwear,),
+                        dbc.Col(footwear,),
+                    ]
                 ),
                 dbc.Row(
-                    [dbc.Col(bottomwear, width=3,), dbc.Col(footwear, width=3,),],
-                    justify="end",
-                ),
-                dbc.Row(
-                    dbc.Col(user_buttons, width=4,), className="m-3", justify="start",
+                    dbc.Col(user_buttons, width={"size": 4, "offset": 4},),
+                    className="m-3",
+                    align="center",
                 ),
                 html.Div(id="app-2-display-value"),
-                dcc.Link("Go to App 1", href="/app1"),
-            ]
+                html.Div([(sidebar), (content),],),
+            ],
         )
-    ]
+    ],
 )
+
 # ------------------------------------- CALLBACKS --------------------------------------
+@app.callback(
+    Output("collapse", "is_open"),
+    [Input("collapse-button", "n_clicks")],
+    [State("collapse", "is_open")],
+)
+def toggle_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
 
 
 app.run_server(
