cff6ce763e5d9503931374836694fbbec5ab49d2
Gabriel Harris <dr.gabriel.harris@gmail.com>
2021-01-18 18:37:36 +0000

    Refactor

--- loginPage.py
+++ loginPage.py
@@ -2,9 +2,7 @@ import base64
 import time
 
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
 import dash_html_components as html
-from dash import callback_context
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 
