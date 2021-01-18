3b804f4c5aa5fa7d350f19cb317e2bac1405bfa4
devd030703 <69916381+devd030703@users.noreply.github.com>
2021-01-18 19:27:30 +0000

    Update imagecarosel.py

--- imagecarosel.py
+++ imagecarosel.py
@@ -2,34 +2,43 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import plotly.graph_objs as go
+import numpy as np
+
+filenames = [
+    "https://raw.githubusercontent.com/plotly/datasets/master/mitochondria.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/6/6c/HeLa_cells_stained_with_Hoechst_33258.jpg",
+]
+
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app = dash.Dash()
-server = app.server
 
-app.layout = html.Div([
+app.layout = html.Div(
+    [
+        html.Div(id="img", className="six columns"),
+        html.Div(id="img2", className="six columns"),
+        html.Button(id="next", n_clicks=0, children="Next", style={"fontSize": 14}),
+        dcc.Interval(id="interval", interval=3000),
+    ]
+)
 
-    html.Section(id="slideshow", children=[
-        html.Div(id="slideshow-container", children=[
-            html.Div(id="image"),
-            dcc.Interval(id='interval', interval=3000)
-        ])
-    ])
 
-])
+@app.callback(Output("img", "children"), [Input("next", "n_clicks")])
+def shownext(clicks):
+    if clicks > 0:
+        print("in shownext")
+        return [html.H3("Triggered by button"), html.Img(src=filenames[clicks % 2])]
 
-@app.callback(Output('image', 'children'),
-              [Input('interval', 'n_intervals')])
+
+@app.callback(Output("img2", "children"), [Input("interval", "n_intervals")])
 def display_image(n):
-    if n == None or n % 3 == 1:
-        img = html.Img(src="http://placeimg.com/625/225/any")
-    elif n % 3 == 2:
-        img = html.Img(src="http://placeimg.com/625/225/animals")
-    elif n % 3 == 0:
-        img = html.Img(src="http://placeimg.com/625/225/arch")
-    else:
-        img = "None"
-    return img
+    if n is None:
+        return dash.no_update
+    print("in interval")
+    return [html.H3("Triggered by interval"), html.Img(src=filenames[n % 2])]
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8868)
