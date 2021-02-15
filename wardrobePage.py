9c6788319620626043f078c681b2cb155322e603
devd030703 <69916381+devd030703@users.noreply.github.com>
2021-02-15 19:39:47 +0000

    update wardrobe

--- wardrobePage.py
+++ wardrobePage.py
@@ -1,47 +1,91 @@
-from inspect import indentsize
+import base64
+from pathlib import Path
 
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from dash_bootstrap_components._components.CardBody import CardBody
-from dash_bootstrap_components._components.Col import Col
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 
 from app import app, database
 
 # ---------------------------------------- DATA ----------------------------------------
 
 # --------------------------------------- IMAGES ---------------------------------------
+def process_image(img):
+    img_encoded = base64.b64encode(open(img, "rb").read())
+    return f"data:image/png;base64,{img_encoded.decode()}"
+
+
+headwear_placeholder_men = process_image(
+    Path("images", "Headwear", "PlaceHolderMen.png"),
+)
+
+topwear_placeholder_men = process_image(
+    Path("images", "Topwear", "PlaceHolderMen.png"),
+)
+
+bottomwear_placeholder_men = process_image(
+    Path("images", "Bottomwear", "PlaceHolderMen.png"),
+)
+
+shoes_placeholder_men = process_image(
+    Path("images", "Shoes", "PlaceHolderMen.png"),
+)
 
 # --------------------------------------- CARDS ----------------------------------------
 headwear = (
     dbc.Card(
         [
-            dbc.CardBody(html.P("Headwear", className="card-text")),
-            dbc.CardImg(
-                src="/Users/devdeepak/Desktop/GitHub/DressSelector/dash/images/2467.jpg",
-                top=True,
+            dbc.CardBody(
+                html.P("Headwear", className="card-text"),
             ),
-            dbc.Row(
-                [
-                    dbc.Button(
-                        children="⬅️",
-                        id="headwear_randomisce",
-                        color="primary",
-                        className="m-3",
-                    ),
-                    dbc.Button(
-                        children="➡️",
-                        id="headwear_cross",
-                        color="primary",
-                        className="m-3",
-                    ),
-                ],
-                justify="center",
+            dbc.CardImg(
+                src=headwear_placeholder_men,
+                id="card_img_headwear",
+                top=True,
             ),
         ]
     ),
 )
 
+
+topwear = (
+    dbc.Card(
+        [
+            dbc.CardBody(html.P("Topwear", className="card-text")),
+            dbc.CardImg(
+                src=topwear_placeholder_men,
+                top=True,
+            ),
+        ]
+    ),
+)
+
+bottomwear = (
+    dbc.Card(
+        [
+            dbc.CardBody(html.P("Bottomwear", className="card-text")),
+            dbc.CardImg(
+                src=bottomwear_placeholder_men,
+                top=True,
+            ),
+        ]
+    ),
+)
+
+
+footwear = (
+    dbc.Card(
+        [
+            dbc.CardBody(html.P("Footwear", className="card-text")),
+            dbc.CardImg(
+                src=shoes_placeholder_men,
+                top=True,
+            ),
+        ]
+    ),
+)
 
 user_buttons = (
     dbc.Card(
@@ -135,7 +179,20 @@ layout = dbc.Container(
                         dbc.Col(
                             headwear,
                         ),
+                        dbc.Col(
+                            topwear,
+                        ),
                     ]
+                ),
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            headwear,
+                        ),
+                        dbc.Col(
+                            topwear,
+                        ),
+                    ],
                 ),
                 dbc.Row(
                     dbc.Col(
