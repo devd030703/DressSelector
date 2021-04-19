df34bf57db8b8fb825b556123bcb421215633c34
Gabriel Harris <dr.gabriel.harris@gmail.com>
2021-04-19 19:38:33 +0100

    Move to MySQL

--- database/create.py
+++ database/create.py
@@ -20,7 +20,7 @@ class DataBase:
         )
 
         with self.engine.connect() as cnxn:
-            cnxn.execute(text("""CREATE DATABASE IF NOT EXISTS database;"""))
+            cnxn.execute(text("""CREATE DATABASE IF NOT EXISTS DEV;"""))
 
         # self.cnxn = self.engine.connect()
 
@@ -33,14 +33,15 @@ class DataBase:
             cnxn.execute(
                 text(
                     """
-        CREATE TABLE "USERS" (
-            "FirstName"	TEXT NOT NULL,
-            "LastName"	TEXT NOT NULL,
-            "Gender"	TEXT NOT NULL,
-            "Email"	TEXT NOT NULL,
-            "Password"	TEXT NOT NULL
-        );
-        """
+                    CREATE TABLE IF NOT EXISTS DEV.USERS (
+                        UserID INT AUTO_INCREMENT PRIMARY KEY,
+                        FirstName TEXT NOT NULL,
+                        LastName TEXT NOT NULL,
+                        Gender TEXT NOT NULL,
+                        Email TEXT NOT NULL,
+                        Password TEXT NOT NULL
+                    );
+                    """
                 )
             )
 
@@ -49,15 +50,15 @@ class DataBase:
             cnxn.execute(
                 text(
                     """
-        CREATE TABLE "ITEMCATALOGUE" (
-            "ItemID" INTEGER NOT NULL,
-            "Subcategory"	TEXT NOT NULL,
-            "Gender"	TEXT NOT NULL,
-            "Season"	TEXT NOT NULL,
-            "Colour"	TEXT NOT NULL,
-            "Image"	BLOB NOT NULL
-        );
-        """
+                    CREATE TABLE IF NOT EXISTS DEV.ITEMCATALOGUE (
+                        ItemID INT AUTO_INCREMENT PRIMARY KEY,
+                        Subcategory	TEXT NOT NULL,
+                        Gender	TEXT NOT NULL,
+                        Season	TEXT NOT NULL,
+                        Colour	TEXT NOT NULL,
+                        Image	BLOB NOT NULL
+                    );
+                     """
                 )
             )
 
@@ -66,14 +67,15 @@ class DataBase:
             cnxn.execute(
                 text(
                     """
-        CREATE TABLE "SAVEDOUTFITS" (
-            "UserID"	INTEGER NOT NULL REFERENCES "USERS"("rowid"),
-            "Headwear"	INTEGER REFERENCES "ITEMCATALOGUE"("ItemID"),
-            "Topwear"	INTEGER REFERENCES "ITEMCATALOGUE"("ItemID"),
-            "Bottomwear"	INTEGER REFERENCES "ITEMCATALOGUE"("ItemID"),
-            "Shoes"	INTEGER REFERENCES "ITEMCATALOGUE"("ItemID")
-        );
-        """
+                    CREATE TABLE IF NOT EXISTS DEV.SAVEDOUTFITS (
+                        OutfitID INT AUTO_INCREMENT PRIMARY KEY,
+                        UserID	INT NOT NULL REFERENCES USERS (UserID),
+                        Headwear	INT REFERENCES ITEMCATALOGUE (ItemID),
+                        Topwear	INT REFERENCES ITEMCATALOGUE (ItemID),
+                        Bottomwear	INT REFERENCES ITEMCATALOGUE (ItemID),
+                        Shoes	INT REFERENCES ITEMCATALOGUE (ItemID)
+                    );
+                    """
                 )
             )
 
@@ -82,12 +84,12 @@ class DataBase:
             cnxn.execute(
                 text(
                     """
-        CREATE TABLE "PREFERENCES" (
-            "UserID"	INTEGER NOT NULL REFERENCES "USERS"("rowid"),
-            "ItemID"	INTEGER NOT NULL REFERENCES "ITEMCATALOGUE"("ItemID"),
-            "Rating"	BOOLEAN NOT NULL
-        );
-        """
+                    CREATE TABLE IF NOT EXISTS DEV.PREFERENCES (
+                        UserID	INT NOT NULL REFERENCES USERS (UserID),
+                        ItemID	INT NOT NULL REFERENCES ITEMCATALOGUE (ItemID),
+                        isLiked	INT NOT NULL
+                    );
+                    """
                 )
             )
 
