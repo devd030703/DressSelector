40c2932dcbeb0022e08413f3cb913a3cc3d9a427
devd030703 <69916381+devd030703@users.noreply.github.com>
2021-04-26 17:33:28 +0100

    comments loginPage.py

--- loginPage.py
+++ loginPage.py
@@ -8,17 +8,22 @@ from dash.exceptions import PreventUpdate
 
 from app import app, database
 
+# ---------------------------------------- DATA ----------------------------------------
+
 # --------------------------------------- IMAGES ---------------------------------------
+# logo
 logo_image = "logo.png"
 ds_logo_encoded = base64.b64encode(open(logo_image, "rb").read())
 ds_logo_decoded = f"data:image/png;base64,{ds_logo_encoded.decode()}"
 
 # --------------------------------------- CARDS ----------------------------------------
+# login card
 login_card = dbc.Card(
     [
         dbc.CardImg(src=ds_logo_decoded, top=True),
         dbc.CardBody(
             [
+                # user name input box
                 dbc.Input(
                     id="input_user_name_login",
                     placeholder="user name",
@@ -26,6 +31,7 @@ login_card = dbc.Card(
                     type="text",
                     className="mb-3",
                 ),
+                # password input box
                 dbc.Input(
                     id="input_password_login",
                     placeholder="password",
@@ -36,6 +42,7 @@ login_card = dbc.Card(
                 dbc.Row(
                     [
                         dbc.Col(
+                            # login button
                             dbc.Button(
                                 children="login",
                                 id="button_login",
@@ -45,6 +52,7 @@ login_card = dbc.Card(
                             width={"size": "auto"},
                         ),
                         dbc.Col(
+                            # signup button
                             dbc.Button(
                                 children="signup",
                                 id="button_signup",
@@ -64,7 +72,7 @@ login_card = dbc.Card(
     color="light",
     inverse=True,
 )
-
+# alert
 alert_login = dbc.Alert(
     id="alert_login",
     dismissable=True,
@@ -99,6 +107,7 @@ layout = dbc.Container(
 
 
 # ------------------------------------- CALLBACKS --------------------------------------
+# AppCallback to validate login
 @app.callback(
     [
         Output("alert_login", "is_open"),
@@ -127,6 +136,7 @@ def validate_login(
                 input_password_login_value is not None
                 and ~input_password_login_value.isspace()
             ):
+                # database method validates the user details
                 if database.check_user_exists(
                     by="email", value=input_user_name_login_value
                 ) and database.check_password_is_correct(
@@ -161,6 +171,7 @@ def validate_login(
 
 
 @app.callback(
+    # changes pathname upon sucessful login
     Output("url", "pathname"),
     [
         Input("button_login", "n_clicks"),
